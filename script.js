369d40646c442cbe22263ca5f9b9642a5b64e063
sriraj2005 <160464187+sriraj2005@users.noreply.github.com>
2024-09-05 13:54:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function appendCharacter(character) {
+    document.getElementById("display").value += character;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function deleteLast() {
+    let display = document.getElementById("display").value;
+    document.getElementById("display").value = display.slice(0, -1);
+}
+
+function calculateResult() {
+    let display = document.getElementById("display").value;
+    try {
+        document.getElementById("display").value = eval(display);
+    } catch (e) {
+        document.getElementById("display").value = "Error";
+    }
+}
+S
